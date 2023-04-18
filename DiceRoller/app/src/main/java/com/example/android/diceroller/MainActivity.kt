8b96034725ef322cfce2b1f8f6e759d313aa87d4
Maikel Bruin <maikelbruin@gmail.com>
2023-04-18 21:02:10 +0200

    fixing app

---
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.diceroller
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroller.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
