6ee2321238a706a3b14ac6703830a7d8d8996094
Maikel Bruin <maikelbruin@gmail.com>
2023-04-19 23:37:10 +0200

    finish layout course

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -34,6 +35,18 @@ class MainActivity : AppCompatActivity() {
             makeColored(it)
         }
 
+        findViewById<Button>(R.id.red_button).setOnClickListener {
+            makeColored(it)
+        }
+
+        findViewById<Button>(R.id.yellow_button).setOnClickListener {
+            makeColored(it)
+        }
+
+        findViewById<Button>(R.id.green_button).setOnClickListener {
+            makeColored(it)
+        }
+
 
     }
 
@@ -48,6 +61,19 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> {
+                findViewById<TextView>(R.id.box_three_text).setBackgroundResource(android.R.color.holo_red_dark)
+            }
+
+            R.id.yellow_button -> {
+                findViewById<TextView>(R.id.box_four_text).setBackgroundResource(android.R.color.holo_orange_light)
+            }
+
+            R.id.green_button -> {
+                findViewById<TextView>(R.id.box_five_text).setBackgroundResource(android.R.color.holo_green_dark)
+            }
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
