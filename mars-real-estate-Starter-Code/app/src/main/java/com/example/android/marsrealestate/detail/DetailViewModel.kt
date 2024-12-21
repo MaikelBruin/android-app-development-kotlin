8923382934e2ca297245e573cf91e7854164a3e3
MB <>
2024-12-21 21:09:23 +0100

    add detail view part 3

--- mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -17,10 +17,14 @@
 package com.example.android.marsrealestate.detail
 
 import android.app.Application
+import android.view.animation.Transformation
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
+import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.network.MarsProperty
 
 /**
@@ -34,5 +38,25 @@ class DetailViewModel(marsProperty: MarsProperty, app: Application) : AndroidVie
 
     init {
         _selectedProperty.value = marsProperty
+    }
+
+    val displayPropertyPrice = selectedProperty.map {
+        app.applicationContext.getString(
+            when (it.isRental) {
+                true -> R.string.display_price_monthly_rental
+                false -> R.string.display_price
+            }, it.price
+        )
+    }
+
+    val displayPropertyType = selectedProperty.map {
+        app.applicationContext.getString(
+            R.string.display_type, app.applicationContext.getString(
+                when (it.isRental) {
+                    true -> R.string.type_rent
+                    false -> R.string.type_sale
+                }
+            )
+        )
     }
 }
