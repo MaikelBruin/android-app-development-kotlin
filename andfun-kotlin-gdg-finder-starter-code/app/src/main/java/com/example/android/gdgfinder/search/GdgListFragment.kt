b431fd6397f168720f4d0b45cb2dd4e9302364bb
MB <>
2024-12-23 21:37:54 +0100

    add chip filters

--- andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ andfun-kotlin-gdg-finder-starter-code/app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
+import com.example.android.gdgfinder.R
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -46,31 +48,39 @@ class GdgListFragment : Fragment() {
         // Sets the adapter of the RecyclerView
         binding.gdgChapterList.adapter = adapter
 
-        viewModel.showNeedLocation.observe(viewLifecycleOwner, object : Observer<Boolean> {
-            override fun onChanged(data: Boolean) {
-                // Snackbar is like Toast but it lets us show forever
-                if (data == true) {
-                    Snackbar.make(
-                        binding.root,
-                        "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
-                        Snackbar.LENGTH_LONG
-                    ).show()
+        viewModel.showNeedLocation.observe(viewLifecycleOwner
+        ) { data -> // Snackbar is like Toast but it lets us show forever
+            if (data == true) {
+                Snackbar.make(
+                    binding.root,
+                    "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
+                    Snackbar.LENGTH_LONG
+                ).show()
 
-                }
             }
-        })
+        }
 
-        viewModel.regionList.observe(viewLifecycleOwner, object : Observer<List<String>> {
-            override fun onChanged(value: List<String>) {
-                val chipGroup = binding.regionList
-                val inflator = LayoutInflater.from(chipGroup.context)
-                val children = value.map { regionName -> {
-                    val chip = inflator.inflate(R.layout)
-                }
+        viewModel.regionList.observe(viewLifecycleOwner
+        ) { value ->
+            val chipGroup = binding.regionList
+            val inflator = LayoutInflater.from(chipGroup.context)
 
+            val children = value.map { regionName ->
+                val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+                chip.text = regionName
+                chip.tag = regionName
+                chip.setOnCheckedChangeListener { button, isChecked ->
+                    viewModel.onFilterChanged(button.tag as String, isChecked)
                 }
+                chip
             }
-        })
+
+            chipGroup.removeAllViews()
+
+            for (chip in children) {
+                chipGroup.addView(chip)
+            }
+        }
 
         setHasOptionsMenu(true)
         return binding.root
